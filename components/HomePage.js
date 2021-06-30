bbe414bfb9fa23762d550002e85e23fe9a5cc334
Romoli Gabriele <gh05romoli@gmail.com>
2021-06-30 16:59:17 +0200

    aggiunte stelline per valutazione

--- components/HomePage.js
+++ components/HomePage.js
@@ -17,7 +17,10 @@ app.component('homepage', {
             <div class="card-body">
                 <h5 class="card-title">{{ movie.name }}</h5>
                 <h5 class="card-title">{{ movie.title }}</h5>
-                <p class="card-text">{{movie.vote_average}}<i class="fas fa-star"></i></p>
+                <p class="card-text"> 
+                    <i v-for="c in calcStar(movie.vote_average)" class="fas fa-star"></i>
+                    <i v-show="mezza" class="fas fa-star-half-alt"></i>
+                </p>
             </div>
         </div>
         </div>
@@ -27,9 +30,23 @@ app.component('homepage', {
     data() {
         return {
             movieList: [],
+            mezza: false
+            
+
             
         }
 
+    },
+
+    methods:{
+        calcStar(x){
+            x = x/2;
+            if (x - Math.round(x) >= 0.35 || x - Math.round(x) <= -0.35) {
+                this.mezza = true;
+                
+            }
+            return Math.round(x)
+        }
     },
     
         
@@ -41,6 +58,7 @@ app.component('homepage', {
             .then(data => {
                 console.log(data);
                 this.movieList = data.results;
+                            
             });
         }else{
             fetch('https://api.themoviedb.org/3/trending/tv/week?api_key=6f9286d54de4891ea7a5c91779e09786')
@@ -49,6 +67,7 @@ app.component('homepage', {
                 console.log(data);
                 this.movieList = data.results;
             });
+            
         }
         
 
