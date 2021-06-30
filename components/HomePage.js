95c1ad2427820da09adcd538460e720f8144e8a5
Romoli Gabriele <gh05romoli@gmail.com>
2021-06-30 17:31:49 +0200

    numero card in base allo schermo

--- components/HomePage.js
+++ components/HomePage.js
@@ -10,7 +10,10 @@ app.component('homepage', {
     template:
         /*html*/
         `
-    <div class="row row-cols-1 row-cols-md-2 g-4">
+
+
+
+    <div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4  g-4">
         <div v-for="movie in movieList"  class="col marg">
         <div class="card ">
             <img :src="'https://image.tmdb.org/t/p/w500/'+movie.poster_path" class="card-img-top" alt="...">
