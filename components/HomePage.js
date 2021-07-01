b5dbaa062c7494e2b99222e6e87024bafdfedadf
Romoli Gabriele <gh05romoli@gmail.com>
2021-07-01 17:54:48 +0200

    descrizione aggiunta

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,12 +1,12 @@
 app.component('homepage', {
-    props:{
-        tipo:{
+    props: {
+        tipo: {
             type: Boolean,
             required: true,
         }
-    }   
+    }
 
-,
+    ,
     template:
         /*html*/
         `
@@ -24,15 +24,18 @@ app.component('homepage', {
                     <i v-for="c in calcStar(movie.vote_average)" class="fas fa-star text-warning"></i>
                     <i v-show="mezza" class="fas fa-star-half-alt text-warning"></i>
                     <i v-for="k in calWstar(movie.vote_average)" class="far fa-star text-warning" ></i>
-                    <p class="ali-r">
                     
+                    <p class="ali-r">
                     <i v-for="e in calcPop(movie.popularity)" class="fas fa-heart text-danger"></i>
                     <i v-show="mezzo" class="fas fa-heart-broken text-danger"></i>
                     <i v-for="g in calcWpop(movie.popularity)" class="far fa-heart text-danger"></i>
-                   
                   </p>
                 </p>
-
+                <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <p>{{movie.overview}}</p>
+                </li>
+                </ul>
             </div>
         </div>
         </div>
@@ -44,23 +47,23 @@ app.component('homepage', {
             movieList: [],
             mezza: false,
             mezzo: false
-            
 
-            
+
+
         }
 
     },
 
-    methods:{
-        calcStar(x){
-            x = x/2;
+    methods: {
+        calcStar(x) {
+            x = x / 2;
             if (x - Math.round(x) >= 0.35 || x - Math.round(x) <= -0.35) {
                 this.mezza = true;
-                
+
             }
             return Math.round(x)
         },
-        calWstar(x){
+        calWstar(x) {
             let y = this.calcStar(x);
             y = 5 - y;
             if (this.mezza) {
@@ -69,49 +72,49 @@ app.component('homepage', {
             return y;
 
         },
-        calcPop(x){
-            x = x/1000;
-            if (x - Math.round(x) >= 0.4  || x - Math.round(x) <= -0.4) {
+        calcPop(x) {
+            x = x / 1000;
+            if (x - Math.round(x) >= 0.4 || x - Math.round(x) <= -0.4) {
                 this.mezzo = true;
-                
-            } 
-            if(Math.round(x)>5){
+
+            }
+            if (Math.round(x) > 5) {
                 return 5
             }
             return Math.round(x)
         },
-        calcWpop(x){
+        calcWpop(x) {
             let y = this.calcPop(x);
 
             y = 5 - y;
-            if(this.mezzo){
+            if (this.mezzo) {
                 y--;
             }
             return y;
         }
     },
-    
-        
+
+
 
     mounted() {
-        if(this.tipo){
+        if (this.tipo) {
             fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=6f9286d54de4891ea7a5c91779e09786')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                this.movieList = data.results;
-                            
-            });
-        }else{
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    this.movieList = data.results;
+
+                });
+        } else {
             fetch('https://api.themoviedb.org/3/trending/tv/week?api_key=6f9286d54de4891ea7a5c91779e09786')
-            .then(response => response.json())
-            .then(data => {
-                console.log(data);
-                this.movieList = data.results;
-            });
-            
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    this.movieList = data.results;
+                });
+
         }
-        
+
 
     },
 
