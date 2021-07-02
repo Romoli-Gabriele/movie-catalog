7e6fc608ebdaaa16cdf9618a923be8f0f422e1d9
Romoli Gabriele <gh05romoli@gmail.com>
2021-07-02 16:48:08 +0200

    aggiunto collapse alle immagini e info extra

--- components/HomePage.js
+++ components/HomePage.js
@@ -10,12 +10,13 @@ app.component('homepage', {
         /*html*/
         `
     <div class="row row-cols-xs-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4  g-5">
-            <trending-card :movieList=" movieList"></trending-card>
+            <trending-card :movieList="movieList" ></trending-card>
     </div>
 `,
     data(){
         return{
             movieList: [],
+            genresList: [],
         }
 
     },
@@ -25,7 +26,6 @@ app.component('homepage', {
         fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=6f9286d54de4891ea7a5c91779e09786')
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 this.movieList = data.results;
 
 
@@ -35,14 +35,16 @@ app.component('homepage', {
         fetch('https://api.themoviedb.org/3/trending/tv/week?api_key=6f9286d54de4891ea7a5c91779e09786')
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 this.movieList = data.results;
             });
 
+               
+        
+            }
     }
 
 
 
-}
+
 
 })
--- components/TrendingCards.js
+++ components/TrendingCards.js
@@ -4,7 +4,8 @@ app.component('trending-card', {
         movieList:{
             type: Array,
             required: true,
-        }
+        },
+       
         
 
     },
@@ -13,7 +14,8 @@ app.component('trending-card', {
         `
     <div v-for="movie in movieList">
         <div  class="card ">
-            <img :src="'https://image.tmdb.org/t/p/w500/'+movie.poster_path" class="card-img-top" alt="...">
+            <img :src="'https://image.tmdb.org/t/p/w500/'+image(calcIndex(movie))" @click="this.imgSwitch(calcIndex(movie))" class="card-img-top" alt="...">
+            
             <div class="card-body">
                 <h5 class="card-title">{{ movie.name }}</h5>
                 <h5 class="card-title">{{ movie.title }}</h5>
@@ -34,7 +36,17 @@ app.component('trending-card', {
                         <button class="no-border" @click="this.show(this.calcIndex(movie))">Show {{this.moreOrLess(this.calcIndex(movie))}}</button>
                         
                     </li>
-                    
+                    <li v-show="this.collImg[this.calcIndex(movie)]" class="list-group-item">
+                        <b>Release date: </b>{{this.convertDate(this.calcIndex(movie))}}
+                        <b>Original language: </b> {{movie.original_language}}
+                    </li>
+                    <li v-show="this.collImg[this.calcIndex(movie)]" class="list-group-item">
+                        <ul >
+                            <b>Genres: </b>
+                            <li v-for="p in movie.genre_ids">{{this.convertGenres(p)}}</li>
+                        </ul>
+
+                    </li>
                 </ul>
                   
                 </p>
@@ -51,17 +63,126 @@ app.component('trending-card', {
     data() {
         return {
             
-            showList: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, ],
+            showList: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            collImg:  [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
             mezza: false, //mezza stella
             mezzo: false, // mezzo cuore
-
-
-
+            genresList: [{"id": 28,"name": "Action"},{"id": 12,"name": "Adventure"},{"id": 16,"name": "Animation"},{"id": 35,"name": "Comedy"},{"id": 80,"name": "Crime"},{"id": 99,"name": "Documentary"},{"id": 18,"name": "Drama"},{"id": 10751,"name": "Family"},{"id": 14,"name": "Fantasy"},{"id": 36,"name": "History"},{"id": 27,"name": "Horror"},{"id": 10402,"name": "Music"},{"id": 9648,"name": "Mystery"},{"id": 10749,"name": "Romance"},{"id": 878,"name": "Science Fiction"},{"id": 10770,"name": "TV Movie"},{"id": 53,"name": "Thriller"},{"id": 10752,"name": "War"},{"id": 37,"name": "Western"}],
+            genresListS: [
+                {
+                    "id": 10759,
+                    "name": "Action & Adventure"
+                },
+                {
+                    "id": 16,
+                    "name": "Animation"
+                },
+                {
+                    "id": 35,
+                    "name": "Comedy"
+                },
+                {
+                    "id": 80,
+                    "name": "Crime"
+                },
+                {
+                    "id": 99,
+                    "name": "Documentary"
+                },
+                {
+                    "id": 18,
+                    "name": "Drama"
+                },
+                {
+                    "id": 10751,
+                    "name": "Family"
+                },
+                {
+                    "id": 10762,
+                    "name": "Kids"
+                },
+                {
+                    "id": 9648,
+                    "name": "Mystery"
+                },
+                {
+                    "id": 10763,
+                    "name": "News"
+                },
+                {
+                    "id": 10764,
+                    "name": "Reality"
+                },
+                {
+                    "id": 10765,
+                    "name": "Sci-Fi & Fantasy"
+                },
+                {
+                    "id": 10766,
+                    "name": "Soap"
+                },
+                {
+                    "id": 10767,
+                    "name": "Talk"
+                },
+                {
+                    "id": 10768,
+                    "name": "War & Politics"
+                },
+                {
+                    "id": 37,
+                    "name": "Western"
+                }
+            ]
         }
 
     },
 
     methods: {
+        convertGenres(p){
+           if(this.movieList[0].media_type == "movie"){
+            for( j = 0; j < 19; j++){
+                if(this.genresList[j].id == p){
+                    return this.genresList[j].name;
+                }
+            }
+           }else{
+            for( j = 0; j < 16; j++){
+                if(this.genresListS[j].id == p){
+                    return this.genresListS[j].name;
+                }
+            }
+
+           }
+            
+            
+        },
+        convertDate(i){
+            let date;
+            if(this.movieList[0].media_type == "movie"){
+                date = this.movieList[i].release_date;
+            }else{
+                date = this.movieList[i].first_air_date;
+            }
+            
+            date = date.split("-").reverse().join("/");
+            return date;
+        },
+        imgSwitch(i){
+            if(this.collImg[i]){
+                this.collImg[i] = false;
+            }else{
+                this.collImg[i] = true;
+            }
+        },
+        image(i){
+            if(this.collImg[i]){
+                return this.movieList[i].backdrop_path;
+            }else{
+                return this.movieList[i].poster_path;
+            }
+
+        },
         calcIndex(m) {
             for (var j = 0; j < 20; j++) {
                 if (m == this.movieList[j]) {
