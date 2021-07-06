5a1bf9e3d97730032c50d880efebb77850be06b1
MiniDivi <alessandro.divincenzo77@gmail.com>
2021-07-06 17:22:41 +0200

    Aggiunto il componente Carousel.vue

--- movie-catalog-vue/src/components/Details.vue
+++ movie-catalog-vue/src/components/Details.vue
@@ -48,55 +48,6 @@
                 </div>
             </div>
             </div>
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-<div class="carousel-indicators">
-  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-</div>
-  <div class="carousel-inner container">
-    <div class="carousel-item active" data-bs-interval="4000">
-    <div class="row">
-    <div class="col-4">
-       <carousel-card :movie="similarList[0]" :type="type"></carousel-card>
-   </div>
-   <div class="col-4">
-   <carousel-card :movie="similarList[1]" :type="type"></carousel-card>
-   </div>                
-   <div class="col-4">
-   <carousel-card :movie="similarList[2]" :type="type"></carousel-card>
-   </div>
-   </div>
-       </div>
-    <div class="carousel-item" data-bs-interval="4000">
-    <div class="row">
-    <div class="col-4">
-       <carousel-card :movie="similarList[3]" :type="type"></carousel-card>
-   </div>
-    <div class="col-4">
-       <carousel-card :movie="similarList[4]" :type="type"></carousel-card>
-   </div>                
-   <div class="col-4">
-        <carousel-card :movie="similarList[5]" :type="type"></carousel-card>
-   </div>
-   </div>    
-   </div>
-    <div class="carousel-item" data-bs-interval="4000">
-    <div class="row">
-    <div class="col-4">
-       <carousel-card :movie="similarList[6]" :type="type"></carousel-card>
-   </div>
-    <div class="col-4">
-       <carousel-card :movie="similarList[7]" :type="type"></carousel-card>
-   </div>                
-   <div class="col-4">
-        <carousel-card :movie="similarList[8]" :type="type"></carousel-card>
-   </div>
-   </div>
-       </div>
-  </div>
-
-</div>
 </template>
 <script>
 import Reviews from "./components/Reviews.vue";
