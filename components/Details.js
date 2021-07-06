f1ad358c94ed2f51c9af3eb9ce076dd73374dee2
Romoli Gabriele <gh05romoli@gmail.com>
2021-07-06 09:35:04 +0200

    non fare danni

--- components/Details.js
+++ components/Details.js
@@ -13,50 +13,58 @@ app.component('dettagli', {
     },
     template:
         /*html*/
-        `
-        <div  class="card bg-dark mb-3">
-            <img :src="'https://image.tmdb.org/t/p/w500/'+movie.poster_path">
-            <div class="card-body">
-            <h5 class="card-title text-danger home-link fs-3">{{ movie.name }}</h5>
-            <h5 class="card-title text-danger home-link fs-3">{{ movie.title }}</h5>
-            <p class="card-title text-light home-link fs-4">{{movie.tagline}}</p>
-            <p class="card-text">
-            <ul class="list-group list-group-flush bg-dark">
-            <li class="list-group-item bg-dark text-light"><b>Review: </b> 
-            <reviews :value="movie.vote_average" :full="'fa-star'" :half="'fa-star-half-alt'" :empty="'fa-star'" :color="'text-warning'" :type="true"></reviews>
-            <br>
-            <b>Number of reviews:</b> {{this.movie.vote_count}}
+        `<div class="container card bg-dark mb-3">
+            <div class="row">
+                <div class="col">
+                    <img :src="'https://image.tmdb.org/t/p/w500/'+movie.poster_path">
+                </div>
+                <div class="col">
+                <div class="card-body">
+                <h5 class="card-title text-danger home-link fs-3">{{ movie.name }}</h5>
+                <h5 class="card-title text-danger home-link fs-3">{{ movie.title }}</h5>
+                <p class="card-title text-light home-link fs-4">{{movie.tagline}}</p>
+                <p class="card-text">
+                <ul class="list-group list-group-flush bg-dark">
+                <li class="list-group-item bg-dark text-light"><b>Review: </b> 
+                <reviews :value="movie.vote_average" :full="'fa-star'" :half="'fa-star-half-alt'" :empty="'fa-star'" :color="'text-warning'" :type="true"></reviews>
+                <br>
+                <b>Number of reviews:</b> {{this.movie.vote_count}}
+                </li>
+            <li class="list-group-item bg-dark text-light"><b>Popularity: </b>
+                <reviews :value="movie.popularity" :full="'fa-heart'" :half="'fa-heart-broken'" :empty="'fa-heart'" :color="'text-danger'" :type="false"></reviews>
             </li>
-        <li class="list-group-item bg-dark text-light"><b>Popularity: </b>
-            <reviews :value="movie.popularity" :full="'fa-heart'" :half="'fa-heart-broken'" :empty="'fa-heart'" :color="'text-danger'" :type="false"></reviews>
-        </li>
-        <li class="list-group-item bg-dark text-light">
-                        <b>Description: </b>
-                        <br>
-                        <p>{{movie.overview}}</p>
+            <li class="list-group-item bg-dark text-light">
+                            <b>Description: </b>
+                            <br>
+                            <p>{{movie.overview}}</p>
+                        </li>
+                    <li class="text-light list-group-item bg-dark">
+                    <b>Status:</b> {{this.movie.status}}
+                    <br>
+                    <b>Release date: </b> {{this.convertDate()}}
+                    <br>
+                    <b>Original language: </b> {{this.language(movie.original_language)}}
+                    <br>
+                    <br>
+                    <br>
+                    <button type="button" class="btn btn-outline-warning "><a :href="movie.homepage" class="mostra-dettagli-button" target="_blank">Watch Now!</a></button>
+                        
                     </li>
-                <li class="text-light list-group-item bg-dark">
-                <b>Status:</b> {{this.movie.status}}
-                <br>
-                <b>Release date: </b> {{this.convertDate()}}
-                <br>
-                <b>Original language: </b> {{this.language(movie.original_language)}}
-                <br>
-                <br>
-                <br>
-                <button type="button" class="btn btn-outline-warning "><a :href="movie.homepage" class="mostra-dettagli-button" target="_blank">Watch Now!</a></button>
-                    
-                </li>
-                <li class="list-group-item bg-dark text-light">
-                    <ul >
-                        <b>Genres: </b>
-                        <li class="bg-dark text-light" v-for="p in movie.genres">{{p.name}}</li>
-                    </ul>
-
-                </li>
-            </ul>
-            </p>
-        </div>
+                    <li class="list-group-item bg-dark text-light">
+                        <ul >
+                            <b>Genres: </b>
+                            <li class="bg-dark text-light" v-for="p in movie.genres">{{p.name}}</li>
+                        </ul>
+    
+                    </li>
+                </ul>
+                </p>
+            </div>
+                </div>
+            </div>
+        
+            
+            
             
            
         </div>
@@ -120,11 +128,11 @@ app.component('dettagli', {
     },
 
     mounted() {
-        fetch('https://api.themoviedb.org/3/'+this.type+'/' +this.idf+ '?api_key=6f9286d54de4891ea7a5c91779e09786&language=en-US')
-        .then(response => response.json())
-        .then(data => {
-            this.movie = data;
-        });
+        fetch('https://api.themoviedb.org/3/' + this.type + '/' + this.idf + '?api_key=6f9286d54de4891ea7a5c91779e09786&language=en-US')
+            .then(response => response.json())
+            .then(data => {
+                this.movie = data;
+            });
 
     }
 
