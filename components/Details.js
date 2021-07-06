bc2a27eec754113bed92fdca7b39bfcaecadf5fe
Romoli Gabriele <gh05romoli@gmail.com>
2021-07-06 09:50:21 +0200

    correlati

--- components/Details.js
+++ components/Details.js
@@ -62,16 +62,38 @@ app.component('dettagli', {
             </div>
                 </div>
             </div>
-        
-            
-            
-            
-           
         </div>
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            
+          </div>
+          <div class="carousel-item">
+            
+          </div>
+          <div class="carousel-item">
+            
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
         `,
     data() {
 
         return {
+            similarList:[],
             movie: null,
             movieList: [],
             mezza: false, //mezza stella
@@ -133,7 +155,12 @@ app.component('dettagli', {
             .then(data => {
                 this.movie = data;
             });
-
+            fetch('https://api.themoviedb.org/3/' + this.type + '/' + this.idf + '/similar?api_key=6f9286d54de4891ea7a5c91779e09786&language=en-US&page=1')
+            .then(response => response.json())
+            .then(data => {
+                this.similarList = data.results;
+            });
+            
     }
 
 
