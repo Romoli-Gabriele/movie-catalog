0ac85009cb49ac781a4384ae627e0be2ac4d23f3
MiniDivi <alessandro.divincenzo77@gmail.com>
2021-07-21 09:41:12 +0200

    modifiche + prima del merge

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -16,58 +16,67 @@ const apiCall = (url, query = {}, options = {}) => {
 }
 
 export const apiService = {
-    getMovieFetch(type, page) {
-        return apiCall(`/trending/${type}/week`, {
-            page
-        })
+        getMovieFetch(type, page) {
+            return apiCall(`/trending/${type}/week`, {
+                page
+            })
 
-    },
-    getSimilar(type, id) {
-        return apiCall(`${type}/${id}/similar`);
-    },
-    getDetail(type, id) {
-        return apiCall(`${type}/${id}`)
-    },
-    getSearch(query) {
-        return apiCall(`search/multi`, {
-            query
-        })
-    },
-    postRate(type, id, voto) {
-        return apiCall(`${type}/${id}/rating`, {session_id: registerService.currentSessionToken}, {
-            method: "post",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                value: voto
-            }),
+        },
+        getSimilar(type, id) {
+            return apiCall(`${type}/${id}/similar`);
+        },
+        getDetail(type, id) {
+            return apiCall(`${type}/${id}`)
+        },
+        getSearch(query) {
+            return apiCall(`search/multi`, {
+                query
+            })
+        },
+        postRate(type, id, voto) {
+            return apiCall(`${type}/${id}/rating`, {
+                session_id: registerService.currentSessionToken
+            }, {
+                method: "post",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    value: voto
+                }),
 
-        }).then()
-    },
+            }).then()
+        },
 
-    doLogin(redirect) {
-        apiCall(`authentication/token/new`).then((data) => {
+        doLogin(redirect) {
+            apiCall(`authentication/token/new`).then((data) => {
 
-            location.href = `https://www.themoviedb.org/authenticate/${data.request_token}?redirect_to=${redirect}`
-        });
-    },
+                location.href = `https://www.themoviedb.org/authenticate/${data.request_token}?redirect_to=${redirect}`
+            });
+        },
 
-    createNewSession() {
-        apiCall(`authentication/session/new`, {}, {
-            method: "post",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                request_token: registerService.currentToken
-            }),
+        createNewSession() {
+            apiCall(`authentication/session/new`, {}, {
+                method: "post",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    request_token: registerService.currentToken
+                }),
 
-        }).then((data) => {
+            }).then((data) => {
 
-            registerService.saveSessionToken(data.session_id)
-        });
-    }
-}
+                registerService.saveSessionToken(data.session_id)
+            });
+        },
+
+        getMyReviews(type) {
+            apiCall(`account/${registerService.currentToken}/rated/${type}`, {
+                    language: languageService.getCurrentLanguage,
+                    session_id: registerService.currentSessionToken
+            }).then()
+            }
+        }
